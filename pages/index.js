d9f42ef475bf8798c207b634585b9f5d0ec3bade
Pieter Montoulieu <pieter@montoulieu.dev>
2020-10-18 11:44:00 -0600

    Initial commit of Next Tailwind Minimal starter

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,37 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col items-center h-screen">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <main className="py-20 flex flex-1 flex-col justify-center items-center">
+        <h1 className="text-6xl">
+          Welcome to <a href="https://nextjs.org" className="text-blue-600 hover:underline">Next.js</a> with <a href="https://tailwindcss.com" className="text-blue-600 hover:underline">Tailwind!</a>
         </h1>
 
-        <p className={styles.description}>
+        <p className="text-center text-2xl leading-normal">
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className="code">pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div className="flex items-center content-center flex-wrap max-w-screen-md mt-12">
+          <a href="https://nextjs.org/docs" className="card">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn" className="card">
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            className="card"
           >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
@@ -40,7 +39,7 @@ export default function Home() {
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="card"
           >
             <h3>Deploy &rarr;</h3>
             <p>
@@ -50,14 +49,23 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="w-full h-24 border-t border-gray-200 flex flex-col justify-center items-center">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
+          className="flex justify-center items-center"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
+        </a>
+        <a
+          href="https://github.com/montoulieu/next-tailwind-minimal"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex justify-center items-center"
+        >
+          Next Tailwind Minimal created by Pieter Montoulieu
         </a>
       </footer>
     </div>
